c391ba69167649b76f47d7fa2636430d2e778c52
Rachel Kelly <racheldk126@gmail.com>
2022-06-21 11:53:52 -0700

    validate user guess

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,24 @@
+computer_word = "dream"
+
 def play_game():
     pass
+
+def get_guess():
+    user_guess = input('Guess a letter: ') 
+    if len(user_guess) >1:
+        print('Your guess is too long.')
+        get_guess()
+    elif user_guess.isalpha:
+        user_guess = user_guess.lower()
+        print(f'You guessed {user_guess}')
+        return user_guess
+    else:    
+        print('Your guess is not a letter.')   
+        get_guess()
+
+get_guess()
+
+
 
 
 if __name__ == "__main__":
