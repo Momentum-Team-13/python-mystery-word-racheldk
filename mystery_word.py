0ef367c086a5027962a758b0a37b4ec0459551ba
Rachel Kelly <racheldk126@gmail.com>
2022-06-22 10:42:42 -0700

    incorrect guesses added to list, counted and game ends after 8

--- mystery_word.py
+++ mystery_word.py
@@ -38,20 +38,24 @@ def play_game():
     # run make_word_into_list and retrieve the thing it made, calling it word_as_list. should be ["d", "r", "e", "a", "m"]
     display = make_display(word_as_list)
     # display = the thing make_display made. should be ["_", "_", "_", "_", "_"] 
+    incorrect_letters = []
     letters_guessed = []
     # will store user_guess letters here if they are not in the word. 
-    while len(letters_guessed) <=8:
+    while len(incorrect_letters) < 8:
         user_guess = get_guess()
         letters_guessed.append(user_guess)
         # user_guess = the thing get_guess made. should be a single, lowercase letter
-        
 
-
-
-
+        if user_guess not in word_as_list:
+            incorrect_letters.append(user_guess)
+            print(f'incorrect guesses: {incorrect_letters}')
 
         display = [(letter.replace(letter, "_")) if letter not in letters_guessed else letter for letter in word_as_list]
         print(f'display {display}')
-
+        
+        # now something to tally incorrect letters 
+        # incorrect_letters = [(letter) if letter not in word_as_list else print() for letter in letters_guessed]
+        # print(f'incorrect_letters {incorrect_letters}')
+    
 if __name__ == "__main__":
     play_game()
